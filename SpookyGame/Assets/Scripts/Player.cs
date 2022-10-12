e76671b1f4e0178d07264c4e8e3b7397ce199f39
aseay4 <86761894+aseay4@users.noreply.github.com>
2022-10-12 10:31:02 -0400

    Possession Prototype

--- SpookyGame/Assets/Scripts/Player.cs
+++ SpookyGame/Assets/Scripts/Player.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    SpriteRenderer spriteRen;
+    public Sprite playerSprite;
     Rigidbody2D rb2d;
     float horizontal;
     float vertical;
     float speed = 5;
+    GameObject possessObject;
+    bool possessed;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -22,6 +27,15 @@ public class Player : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical");
 
         Vector2 move = new Vector2(horizontal, vertical);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (possessObject != null)
+            {
+                spriteRen.sprite = null;
+                possessed = true;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -31,5 +45,24 @@ public class Player : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rb2d.MovePosition(position);
+
+        if (possessed == true)
+        {
+            Rigidbody2D objRb2d = possessObject.GetComponent<Rigidbody2D>();
+            Vector2 objPosition = objRb2d.position;
+            possessObject.transform.parent = transform;
+
+            objPosition.x = possessObject.transform.parent.position.x + speed * horizontal * Time.deltaTime;
+            objPosition.y = possessObject.transform.parent.position.x + speed * horizontal * Time.deltaTime;
+            objRb2d.MovePosition(position);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "CanPossess")
+        {
+            possessObject = collision.gameObject;
+        }
     }
 }
