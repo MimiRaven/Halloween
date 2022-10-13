b9d7728c1c616651e0e762cca31bd14a81d31239
aseay4 <86761894+aseay4@users.noreply.github.com>
2022-10-12 21:45:32 -0400

    Player Controller Fixed

--- SpookyGame/Assets/Scripts/PlayerController.cs
+++ SpookyGame/Assets/Scripts/PlayerController.cs
@@ -5,26 +5,87 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    SpriteRenderer spriteRen;
+    public Sprite playerSprite;
     private Vector2 movement;
-    private Rigidbody2D rb;
+    private Rigidbody2D rb2d;
     public int speed = 5;
+    GameObject possessObject;
+    bool possessed;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
     }
 
-    private void OnMovement(InputValue value)
+    private void OnMovement(InputValue value) // These are Movement Keys
     {
         movement = value.Get<Vector2>();
+    }
+    private void OnPossession() // This IS the Space Key
+    {
+        if (possessed == false && possessObject != null)
+        {
+            PossessObject p = possessObject.GetComponent<PossessObject>();
+            p.disableCollider();
+
+            spriteRen.sprite = null;
+            possessed = true;
+        }
+
+        else if (possessed == true)
+        {
+            PossessedObjAction();
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-      //  if(movement.x != 0 || movement.y != 0)
-      //  {
-      //      rb.velocity = movement * speed;
-      //  }
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+
+        if (possessed == true)
+        {
+            PossessedObjMovement();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "CanPossess")
+        {
+            possessObject = collision.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "CanPossess")
+        {
+            possessObject = null;
+        }
+    }
+
+    void PossessedObjMovement()
+    {
+        Vector2 position = rb2d.position;
+        Rigidbody2D objRb2d = possessObject.GetComponent<Rigidbody2D>();
+        Vector2 objPosition = objRb2d.position;
+        possessObject.transform.parent = transform;
+
+        // objPosition.x = possessObject.transform.parent.position.x + speed * horizontal * Time.deltaTime;
+        // objPosition.y = possessObject.transform.parent.position.x + speed * horizontal * Time.deltaTime;
+        objRb2d.MovePosition(position);
+    }
+
+    void PossessedObjAction()
+    {
+        // Can implement functionality like throw object, flicker light, explode, etc... 
+        PossessObject p = possessObject.GetComponent<PossessObject>();
+        p.enableCollider();
+
+        spriteRen.sprite = playerSprite;
+        possessed = false;
+        possessObject = null;
     }
 }
