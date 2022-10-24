00717cf8627f6795285ec27254c01248248eb76b
Puncakeboy <86800954+Puncakeboy@users.noreply.github.com>
2022-10-24 15:48:54 -0400

    FixedPlayerBounds,FixedCameraBounds,AdjustedPlayerCollider,ProgressBarNeedsCode

---
+++ SpookyGame/Assets/Scripts/Progress.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progress : MonoBehaviour
+{
+    public static Progress instance { get; private set; }
+
+    public Image mask;
+    float originalSize;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.height;
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, originalSize * value);
+    }
+}
